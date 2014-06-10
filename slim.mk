56eb4d9ed3faf363692a56a89e0394e4b2f67d75
vocoderism <davide.franzino@gmail.com>
2014-06-10 22:33:58 +0200

    CRoMize

--- slim.mk
+++ slim.mk
@@ -5,11 +5,12 @@ TARGET_SCREEN_WIDTH := 720
 # Inherit device configuration
 $(call inherit-product, device/samsung/toroplus/full_toroplus.mk)
 
-# Inherit some common stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+# Inherit CRoM common stuff.
+$(call inherit-product, vendor/crom/config/common_phone.mk)
 
-# Inherit CDMA common stuff	11
-$(call inherit-product, vendor/slim/config/cdma.mk)
+# bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/crom/prebuilt/common/media/xhdpi/BOOTANIMATION-1280x768.zip:system/media/bootanimation.zip
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.camera.res=5MP
@@ -20,7 +21,7 @@ PRODUCT_RELEASE_NAME := toroplus
 PRODUCT_BUILD_PROP_OVERRIDES +=  PRODUCT_NAME=mysidspr BUILD_FINGERPRINT="samsung/mysidspr/toroplus:4.2.1/JOP40D/eng.ar.20121202.005722:userdebug/release-keys"
 PRIVATE_BUILD_DESC="mysidspr-userdebug 4.2.1 JOP40D 005722 release-keys"
 
-PRODUCT_NAME := slim_toroplus
+PRODUCT_NAME := crom_toroplus
 PRODUCT_DEVICE := toroplus
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Galaxy Nexus
