e8f360e34f4c18a34e203ec85fea09c85217d20a
vocoderism <davide.franzino@gmail.com>
2014-06-24 09:52:27 +0200

    Update crom.mk

--- crom.mk
+++ crom.mk
@@ -1,7 +1,6 @@
 # Boot animation
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 720
-
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
 # Inherit device configuration
 $(call inherit-product, device/samsung/toroplus/full_toroplus.mk)
 
